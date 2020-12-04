1f9c7eadd5ed5ceea0c661b8c61d935b40ca1993
KanayaNadira <75061607+KanayaNadira@users.noreply.github.com>
2020-12-04 16:15:28 +0700

    Add files via upload

---
+++ data/IUP_1.js
@@ -0,0 +1,1 @@
+var json_IUP_1 = {"type":"FeatureCollection","name":"IUP_1","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1"},"geometry":{"type":"Point","coordinates":[107.30031307116829,-6.305112614586402]}}]}
